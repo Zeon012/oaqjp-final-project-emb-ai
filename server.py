47a51eee7b89adaa9187c9f069f61f619c42db2d
Matthew van Winkle <mvanwi3@lsu.edu>
2025-05-20 13:27:54 -0500

    modified:   server.py

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
+''' Launch a server on port 5000 to interface with the Emotion Detector'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-import json
-
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def sent_analyzer():
+def em_detector():
+    ''' Call the emotion_detector function'''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
@@ -23,7 +23,9 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
+    ''' Render the index.html file'''
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+# The above code is a Flask web application that provides an interface for emotion detection using a pre-trained model.
