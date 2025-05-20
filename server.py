7e4df56946cae00153f7ff4a276bca713c760ce4
Matthew van Winkle <mvanwi3@lsu.edu>
2025-05-20 13:05:41 -0500

    modified:   server.py

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
-
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 import json
 
 
-app=Flask("Emotion Detector")
+app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
     return (
         f"For the given statement, the system response is: "
         f"'anger': {response['anger']}, "
