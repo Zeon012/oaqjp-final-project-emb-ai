a6cecf89f1d159ea72e9f500c57c49b0726a39a3
Matthew van Winkle <mvanwi3@lsu.edu>
2025-05-20 12:57:49 -0500

    modified:   server.py 	modified:   templates/index.html

--- server.py
+++ server.py
@@ -1,7 +1,3 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
 
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
@@ -26,9 +22,6 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
