9934d21f8c30a232af930700a275abbb74e6dc90
Matthew Van Winkle <mvanwinkle0818@outlook.com>
2025-05-20 11:44:13 -0400

    Initial commit

---
+++ server.py
@@ -0,0 +1,30 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+import json
+
+
+app=Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    formatted_response=json.loads(response.text) #Parsing Json Text
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    return "For the given statement the system response is, " ,"'anger': emotions['anger']","'disgust': emotions['disgust']","'fear': emotions['fear']", "'joy': emotions['joy']", "'sadness': emotions['sadness']," "and the dominant emotion is ", dominant_emotion
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
