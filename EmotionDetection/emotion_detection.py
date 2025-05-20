d3f06a724d8bf952a201f0305e31fdf431631655
Matthew van Winkle <mvanwi3@lsu.edu>
2025-05-20 13:03:07 -0500

    modified:   EmotionDetection/emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,6 +5,17 @@ def emotion_detector(text_to_analyse):  # Define a function named emotion_detect
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+    
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    
     formatted_response=json.loads(response.text) #Parsing Json Text
     emotions = formatted_response['emotionPredictions'][0]['emotion']
     dominant_emotion = max(emotions, key=emotions.get)
